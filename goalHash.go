103b444792b5374816f7bf8a47a22ae581583a51
@Hari-Kiri <pratamahari89@gmail.com>
2022-10-13 21:54:39 +0700

    Add sha512 hash function.

--- goalHash.go
+++ goalHash.go
@@ -2,13 +2,35 @@ package goalHash
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
+	"fmt"
 )
 
 // Hash string to sha256
 func Sha256(content string) string {
-	hashPasswordToSha256 := sha256.Sum256([]byte(content))
-	passwordHashed := base64.StdEncoding.EncodeToString(hashPasswordToSha256[:])
-	return passwordHashed
+	hash := sha256.New()
+	hash.Write([]byte(content))
+	return fmt.Sprintf("%x", hash.Sum(nil))
 }
 
+// Hash string to sha256 and encode with base64
+func Sha256WithBase64(content string) string {
+	hash := sha256.New()
+	hash.Write([]byte(content))
+	return base64.StdEncoding.EncodeToString(hash.Sum(nil))
+}
+
+// Hash string to sha512
+func Sha512(content string) string {
+	hash := sha512.New()
+	hash.Write([]byte(content))
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+// Hash string to sha256 and encode with base64
+func Sha512WithBase64(content string) string {
+	hash := sha512.New()
+	hash.Write([]byte(content))
+	return base64.StdEncoding.EncodeToString(hash.Sum(nil))
+}
